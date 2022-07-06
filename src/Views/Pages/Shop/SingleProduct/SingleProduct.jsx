8eee78051acd785197d29cfa9668e55d6e825cca
Jahid0178 <jahidulhaque767@gmail.com>
2022-07-07 04:53:33 +0600

    install react rating

--- src/Views/Pages/Shop/SingleProduct/SingleProduct.jsx
+++ src/Views/Pages/Shop/SingleProduct/SingleProduct.jsx
@@ -24,7 +24,8 @@ const SingleProduct = () => {
         setGetProduct(data.find((item) => item.productId == productId))
       );
   });
-  const { img, productName, productDetails, price } = getProduct;
+  const { img, productName, productDetails, price, description, rating } =
+    getProduct;
   return (
     <div>
       <div className="container mx-auto px-3">
@@ -36,8 +37,9 @@ const SingleProduct = () => {
             <h2 className="text-orange-500 font-semibold text-3xl uppercase mb-2">
               {productName}
             </h2>
+            <p>{rating}</p>
             <p className="py-3 text-lg">{productDetails}</p>
-            <p className="text-lg mb-3 text-orange-500 font-medium">
+            <p className="text-3xl mb-3 text-orange-500 font-medium">
               $ {price}
             </p>
             <div className="mt-4">
@@ -55,13 +57,21 @@ const SingleProduct = () => {
               />
             </div>
             <div className="mt-2">
-              <h4 className="text-lg font-bold text-orange-500">
+              <h4 className="text-2xl font-bold text-orange-500">
                 Total Quantity Price
               </h4>
-              <p className="font-bold text-orange-500">$ {price * quantity}</p>
+              <p className="font-bold text-orange-500 text-2xl">
+                $ {price * quantity}
+              </p>
             </div>
             <button className="btn-secondary mt-5">add to cart</button>
           </div>
+        </div>
+        <div className="p-5">
+          <h3 className="text-3xl font-semibold text-center mb-2 text-orange-500">
+            Description
+          </h3>
+          <p className="leading-10">{description}</p>
         </div>
       </div>
     </div>
