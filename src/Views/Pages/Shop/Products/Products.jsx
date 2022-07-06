2d323d6899a03762842f76f7d4ada71c59908075
Jahid0178 <jahidulhaque767@gmail.com>
2022-07-07 03:14:09 +0600

    show product on product details

--- src/Views/Pages/Shop/Products/Products.jsx
+++ src/Views/Pages/Shop/Products/Products.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Products = ({ product }) => {
-  const { img, productName, price } = product;
+  const { productId, img, productName, price } = product;
   return (
     <div className="container mx-auto px-3">
-      <div className="text-center">
+      <div className="text-center product-container">
         <img src={img} alt="Product_Image" />
         <h2 className="text-2xl font-semibolduppercase uppercase font-semibold">
           {productName}
         </h2>
         <p className="font-semibold text-lg text-orange-500">$ {price}</p>
-        <Link to="#" className="btn-secondary mt-5 hover:bg-black inline-block">
+        <Link
+          to={`/shop/${productId}`}
+          className="btn-secondary mt-5 hover:bg-black inline-block"
+        >
           add to cart
         </Link>
       </div>
--- src/Views/Pages/Shop/SingleProduct/SingleProduct.jsx
+++ src/Views/Pages/Shop/SingleProduct/SingleProduct.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const [getProduct, setGetProduct] = useState({});
+  useEffect(() => {
+    const url = "/fakeData.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) =>
+        setGetProduct(data.find((item) => item.productId == productId))
+      );
+  });
+  const { img, productName, productDetails, price } = getProduct;
   return (
     <div>
       <div className="container mx-auto px-3">
-        <h1>This is single product</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-5 gap-4">
+          <div>
+            <img src={img} alt="Product Name" />
+          </div>
+          <div>
+            <h2 className="text-orange-500 font-semibold text-3xl uppercase mb-2">
+              {productName}
+            </h2>
+            <p className="py-3 text-lg">{productDetails}</p>
+            <p className="text-lg mb-3 text-orange-500 font-medium">
+              $ {price}
+            </p>
+            <p>Quantity</p>
+            <button className="btn-secondary mt-5">add to cart</button>
+          </div>
+        </div>
       </div>
     </div>
   );
